8c1d4edceee96fb607f73bbbb4373160955fb1bf
Daniel Hawkins <SilverNexus@users.noreply.github.com>
2014-05-22 17:51:56 +0000

    Added support for multiple root directories in search.

--- settings.c
+++ settings.c
@@ -1,7 +1,7 @@
 /***************************************************************************/
 /*                                                                         */
 /*                               settings.c                                */
-/* Original code written by Daniel Hawkins. Last modified on 2014-05-21.   */
+/* Original code written by Daniel Hawkins. Last modified on 2014-05-22.   */
 /*                                                                         */
 /* The file defines the functions for handling settings.                   */
 /*                                                                         */
@@ -13,33 +13,54 @@
 
 int init_settings(){
     settings.excluded_directories = 0;
-    settings.root_dir = 0;
+    settings.root_dirs = 0;
     return 0;
 }
 
 int free_settings(){
     while (settings.excluded_directories){
-        struct exclude_dir *tmp = settings.excluded_directories;
+        struct dir_list *tmp = settings.excluded_directories;
         settings.excluded_directories = settings.excluded_directories->next;
         free(tmp);
     }
     settings.excluded_directories = 0;
-    settings.root_dir = 0;
+    while (settings.root_dirs){
+        struct dir_list *tmp = settings.root_dirs;
+        settings.root_dirs = settings.root_dirs->next;
+        free(tmp);
+    }
+    settings.root_dirs = 0;
     return 0;
 }
-int add_exclude_dir(char *dir){
-    struct exclude_dir *newExclusion = (struct exclude_dir *)malloc(sizeof(struct exclude_dir));
+int add_exclude_dir(char *newDir){
+    struct dir_list *newExclusion = (struct dir_list *)malloc(sizeof(struct dir_list));
     if (!newExclusion){
-        logError(ERROR, "Could not allocate space for excluded directory.");
+        logError(ERROR, "Could not allocate space for excluded directory %s.", newDir);
         return -1;
     }
 
     /*
-     * Since dir is an argument passed into the program, in should stay allocated for
-     * the duration of the program. Thus, just assign the reference in exclude_dir.
+     * Since newDir is an argument passed into the program, in should stay allocated for
+     * the duration of the program. Thus, just assign the reference in dir_list.
      */
-    newExclusion->excludeDir = dir;
+    newExclusion->dir = newDir;
     newExclusion->next = settings.excluded_directories;
     settings.excluded_directories = newExclusion;
+    return 0;
+}
+int add_root_dir(char *newDir){
+    struct dir_list *newRoot = (struct dir_list *)malloc(sizeof(struct dir_list));
+    if (!newRoot){
+        logError(ERROR, "Could not allocate space for root directory %s.", newDir);
+        return -1;
+    }
+
+    /*
+     * Since newDir is an argument passed into the program, in should stay allocated for
+     * the duration of the program. Thus, just assign the reference in dir_list.
+     */
+    newRoot->dir = newDir;
+    newRoot->next = settings.root_dirs;
+    settings.root_dirs = newRoot;
     return 0;
 }
