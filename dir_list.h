d5d1839cd2aa34249628ef9c3f3147ebe752f2b6
SilverNexus <a_daniel.hawkins@yahoo.com>
2016-05-25 10:29:49 -0400

    Merge DIR_LIST typedef into struct declaration.

--- dir_list.h
+++ dir_list.h
@@ -6,13 +6,10 @@
 #ifndef DIR_LIST_H
 #define DIR_LIST_H
 
-struct dir_list{
+typedef struct dir_list{
     char *dir;
     struct dir_list *next;
-};
-
-// It seems to be normal to make structure types in all caps in C.
-typedef struct dir_list DIR_LIST;
+} DIR_LIST;
 
 DIR_LIST *init_dir_node(char *);
 int link_dir_node(DIR_LIST *, DIR_LIST **);
