83eec2cae3ccfc70981f2d8b8f7751b705f85d9d
SilverNexus <a_daniel.hawkins@yahoo.com>
2016-06-01 17:32:35 -0400

    Add tests to ensure proper functioning of the new binary file search functions.

--- test/search_test/test_search_results.sh
+++ test/search_test/test_search_results.sh
@@ -45,6 +45,31 @@ end
 ../../fileperuser -o script_single_longend -1 -d test_file "ons
 end
 "
+# Add binary searches to the list. Current implementation of binary file detection
+# should not affect the single vs. multi match.
+../../fileperuser -o script_binary_nocase_short -b -n -d test_file p
+../../fileperuser -o script_binary_nocase_medium -b -n -d test_file po
+../../fileperuser -o script_binary_nocase_long -b -n -d test_file "haystack_last"
+../../fileperuser -o script_binary_short -b -d test_file ";"
+../../fileperuser -o script_binary_medium -b -d test_file try
+../../fileperuser -o script_binary_long -b -d test_file creation
+
+# Do more test cases on the end.
+../../fileperuser -o script_binary_nocase_shortend -b -n -d test_file "
+"
+../../fileperuser -o script_binary_nocase_testend -b -n -d test_file "end
+"
+../../fileperuser -o script_binary_nocase_longend -b -n -d test_file "ons
+end
+"
+../../fileperuser -o script_binary_shortend -b -d test_file "
+"
+../../fileperuser -o script_binary_testend -b -d test_file "end
+"
+../../fileperuser -o script_binary_longend -b -d test_file "ons
+end
+"
+
 
 # Compile the differences between the actual results and the expected results
 diff script_results1 test_results1 > diff_file
@@ -60,6 +85,12 @@ diff script_single_results3 test_single_results3 >> diff_file
 diff script_single_nocase_results1 test_single_nocase_results1 >> diff_file
 diff script_single_nocase_results2 test_single_nocase_results2 >> diff_file
 diff script_single_nocase_results3 test_single_nocase_results3 >> diff_file
+diff script_binary_nocase_short test_binary_nocase_short >> diff_file
+diff script_binary_nocase_medium test_binary_nocase_medium >> diff_file
+diff script_binary_nocase_long test_binary_nocase_long >> diff_file
+diff script_binary_short test_binary_short >> diff_file
+diff script_binary_medium test_binary_medium >> diff_file
+diff script_binary_long test_binary_long >> diff_file
 # And these for the end case
 diff script_testend test_testend >> diff_file
 diff script_single_testend test_single_testend >> diff_file
@@ -71,6 +102,12 @@ diff script_nocase_shortend test_nocase_shortend >> diff_file
 diff script_single_nocase_shortend test_single_nocase_shortend >> diff_file
 diff script_longend test_longend >> diff_file
 diff script_single_longend test_single_longend >> diff_file
+diff script_binary_nocase_shortend test_binary_nocase_shortend >> diff_file
+diff script_binary_nocase_testend test_binary_nocase_testend >> diff_file
+diff script_binary_nocase_longend test_binary_nocase_longend >> diff_file
+diff script_binary_shortend test_binary_shortend >> diff_file
+diff script_binary_testend test_binary_testend >> diff_file
+diff script_binary_longend test_binary_longend >> diff_file
 
 # Show the differences
 RES=`cat diff_file`
@@ -87,6 +124,10 @@ script_nocase_results2 script_nocase_results3 script_single_results1 script_sing
 script_single_results3 script_single_nocase_results1 script_single_nocase_results2 \
 script_single_nocase_results3 script_testend script_single_testend script_nocase_testend \
 script_single_nocase_testend script_shortend script_single_shortend script_nocase_shortend \
-script_single_nocase_shortend script_longend script_single_longend diff_file
+script_single_nocase_shortend script_longend script_single_longend script_binary_short \
+script_binary_nocase_short script_binary_shortend script_binary_nocase_shortend \
+script_binary_medium script_binary_nocase_medium script_binary_testend \
+script_binary_nocase_testend script_binary_long script_binary_nocase_long \
+script_binary_longend script_binary_nocase_longend diff_file
 # exit successfully
 exit 0
